f323085ed01270bcc8a25d6d799ee14b7f994354
Romain Bessuges-Meusy <romain.bessuges@main-gauche.com>
2020-06-09 16:38:37 +0200

    Init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Headless CMS comparator`,
+    description: `Inception-minded project that aims to compare and understand headless CMS features and vendors`,
+    author: `@rbessuges`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,6 +15,14 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+          spaceId: `nnyhgu3ljybh`,
+          // Learn about environment variables: https://gatsby.dev/env-vars
+          accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN,
+      }
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
